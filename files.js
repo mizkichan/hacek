518869218a7ac8872c14ab23f7184876045e6fcf
Mizuki HAGIMOTO <hagimotomizuki@gmail.com>
2018-11-08 18:35:06 +0900

    Updates

--- files.js
+++ files.js
@@ -1,24 +1,31 @@
 var files =
 [
     [ "alloc.c", "alloc_8c_source.html", null ],
+    [ "alloc.d", "alloc_8d_source.html", null ],
     [ "alloc.h", "alloc_8h_source.html", null ],
     [ "defs.h", "defs_8h_source.html", null ],
     [ "error.c", "error_8c_source.html", null ],
+    [ "error.d", "error_8d_source.html", null ],
     [ "error.h", "error_8h_source.html", null ],
     [ "hacek.c", "hacek_8c_source.html", null ],
     [ "hoge.c", "hoge_8c_source.html", null ],
     [ "lexer.c", "lexer_8c_source.html", null ],
     [ "lexer.h", "lexer_8h_source.html", null ],
     [ "lexer_test.c", "lexer__test_8c_source.html", null ],
+    [ "lexer_test.d", "lexer__test_8d_source.html", null ],
     [ "parser.c", "parser_8c_source.html", null ],
     [ "parser.h", "parser_8h_source.html", null ],
     [ "preprocessor.c", "preprocessor_8c_source.html", null ],
     [ "preprocessor.h", "preprocessor_8h_source.html", null ],
     [ "preprocessor_test.c", "preprocessor__test_8c_source.html", null ],
+    [ "preprocessor_test.d", "preprocessor__test_8d_source.html", null ],
     [ "test.c", "test_8c_source.html", null ],
+    [ "test.d", "test_8d_source.html", null ],
     [ "token.c", "token_8c_source.html", null ],
+    [ "token.d", "token_8d_source.html", null ],
     [ "token.h", "token_8h_source.html", null ],
     [ "utils.c", "utils_8c_source.html", null ],
     [ "utils.h", "utils_8h_source.html", null ],
-    [ "utils_test.c", "utils__test_8c_source.html", null ]
+    [ "utils_test.c", "utils__test_8c_source.html", null ],
+    [ "utils_test.d", "utils__test_8d_source.html", null ]
 ];
--- structIntegerConstant.js
+++ structIntegerConstant.js
@@ -1,5 +1,19 @@
 var structIntegerConstant =
 [
+    [ "IntegerConstantType", "structIntegerConstant.html#a147c6f22313b4defb87f1eabc67c5b55", [
+      [ "INTEGER_CONSTANT_INT", "structIntegerConstant.html#a147c6f22313b4defb87f1eabc67c5b55a6d5b7c2c4e5c18f2b15f4ac6e31ebb4b", null ],
+      [ "INTEGER_CONSTANT_UINT", "structIntegerConstant.html#a147c6f22313b4defb87f1eabc67c5b55acc637f7c2fa3abe842e02829d6d419a0", null ],
+      [ "INTEGER_CONSTANT_LONG", "structIntegerConstant.html#a147c6f22313b4defb87f1eabc67c5b55ae1d8e094be427013a016c2250671df06", null ],
+      [ "INTEGER_CONSTANT_ULONG", "structIntegerConstant.html#a147c6f22313b4defb87f1eabc67c5b55a164a62ca6ab6c2af56cf1342a115dbe8", null ],
+      [ "INTEGER_CONSTANT_LLONG", "structIntegerConstant.html#a147c6f22313b4defb87f1eabc67c5b55a4f6df1d9a23ed5844a383d26c514d4d8", null ],
+      [ "INTEGER_CONSTANT_ULLONG", "structIntegerConstant.html#a147c6f22313b4defb87f1eabc67c5b55ab08eae1ae040ae69399ae45a5c1d8433", null ]
+    ] ],
     [ "pos", "structIntegerConstant.html#aaaca1f5838dea7e341477e1a5ee87094", null ],
-    [ "value", "structIntegerConstant.html#a20ca3ad82d733317185ad63768c56587", null ]
+    [ "type", "structIntegerConstant.html#ae0e3e958c5df6f000b580a32d4c89f54", null ],
+    [ "value_int", "structIntegerConstant.html#af6f65850058c5e403195cf64a6927f84", null ],
+    [ "value_llong", "structIntegerConstant.html#a50e08d21c54fac30e964943a79476c3c", null ],
+    [ "value_long", "structIntegerConstant.html#a12c9d483c2ef28bad275429eb84e6866", null ],
+    [ "value_uint", "structIntegerConstant.html#a75234e7745cdcc60060f10fda48d031a", null ],
+    [ "value_ullong", "structIntegerConstant.html#a03a3a09b75b496d1145d00fe417fb850", null ],
+    [ "value_ulong", "structIntegerConstant.html#a427dfb3485a74fe60df8518d6a906b51", null ]
 ];
